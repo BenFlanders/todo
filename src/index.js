ba2b93dfed17c2bb3065d2b6e7b704aa6daf1b30
Ben Flanders <benjamin.flanders96@gmail.com>
2020-07-24 14:22:09 -0400

    Uploaded finished code with working MySQL database connection

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql2');
 
 const api = express();
 
@@ -7,10 +8,25 @@ api.use(express.static(__dirname + '/public'));
 
 api.use(bodyParser.json());
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'BNFKAWF2019!',
+    database: 'todo'
+});
+
 api.listen(3000, () => {
     console.log('API up and running on port 3000!');
 });
 
 api.post('/add', (req, res) => {
-    res.send('It works!')
-});
+    console.log(req.body);
+    
+    connection.query('INSERT INTO tasks (description) VALUES (?)', [req.body.item], (error, results) => {
+     if (error) return res.json({ error: error });
+   connection.query('SELECT LAST_INSERT_ID() FROM tasks', (error, results) => {
+      if (error) return res.json({ error: error });
+   console.log(results);
+     });
+    });
+   });
